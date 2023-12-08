c7c6053a182a84ba3936d2e75db6b59bed53f70c
8467042733 <rupeshpal25112004@gmail.com>
2023-12-08 14:08:29 +0530

    changes code

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,46 @@
 "use client"
-import { useState } from 'react';
-import FactorialComponent from '../components/FactorialComponent';
-export default function Home() {
+// import { useState } from 'react';
+// import FactorialComponent from '../components/FactorialComponent';
+// export default function Home() {
   
      
+//   return (
+//     <div>
+//         <h1>Welcome to Next.js!</h1>
+//         <section>
+//                 <h2>Factorial Calculation</h2>
+//                 <input 
+//                     type="number" 
+                    
+//                    } 
+//                 />
+       
+//                 <FactorialComponent  />
+//             </section>
+//     </div>
+// );
+// }
+
+// pages/index.js
+import { useState } from 'react';
+import FactorialComponent from '../components/FactorialComponent';
+
+export default function Home() {
+  const [inputValue, setInputValue] = useState(0);
+
   return (
     <div>
-        <h1>Welcome to Next.js!</h1>
-        <section>
-                <h2>Factorial Calculation</h2>
-                <input 
-                    type="number" 
-                    
-                   } 
-                />
-       
-                <FactorialComponent  />
-            </section>
+      <h1>Welcome to Next.js!</h1>
+      <section>
+        <h2>Factorial Calculation</h2>
+        <input
+          type="number"
+          value={inputValue}
+          onChange={(e) => setInputValue(parseInt(e.target.value))}
+        />
+        <FactorialComponent number={inputValue} />
+      </section>
     </div>
-);
+  );
 }
+
